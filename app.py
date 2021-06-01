ef7dcf07e6aef507d0dbcac7b96811dd39a261e8
gdorzilme <gdorzilme@gmail.com>
2021-05-31 21:13:08 -0400

    Update Flask app build

--- app.py
+++ app.py
@@ -2,7 +2,30 @@ import requests
 import configparser
 from flask import Flask, render_template, request
 
+app = Flask (__name__)
 
+@app.route('/')   #Calls the function and render it at the homepage URL
+def weather_dashboard():
+    return render_template('home.html')
+
+
+@app.route('/results', methods=['POST'])
+def render_results():
+    cityname = request.form['cityName']  #variable coming from Flask
+    api_key = get_api_key()
+
+    data = get_weather_results(cityname, api_key)  #Pass in the city and API key
+    temp = "{0:.2f}".format(data["main"]["temp"]) #formatted temperature to 2 decimal places
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])  #formatted feels like temp to 2 decimal places
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read(f'config.ini')
+    return config['openweathermap']['api']
 
 
 # Open Weather App API function
